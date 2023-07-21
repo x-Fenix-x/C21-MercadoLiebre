3ca9cdb4cb94d15ca4aaaa4fe80bb4bd24e8a1ad
unknown <vallejosr@protonmail.com>
2023-07-21 11:06:30 -0300

    feat: formularios registro & login

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 Se agrega los formularios de registro y login a Mercado Liebre.
 
+<p align="center">
+  <img src="/mocks/mobile.gif" alt="Texto alternativo">
+</p>
+
 ### Autor: R. Vallejos
--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const path = require('path');
 const PORT = 3030;
 
-app.use(express.static(path.join(__dirname,'public')));
+/* Configuraciones */
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views', 'home.html'));
-});
+/* Rutas */
 
-app.listen(PORT, () =>
-  console.log(`Server corriendo en http://localhost:${PORT}`)
-);
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
+
+app.listen(PORT, () =>console.log(`Server corriendo en http://localhost:${PORT}`));
